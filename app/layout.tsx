00fb3bea4cbeb8e780e28a734b510984635251af
Abhinav Rawat <abhinav63381@gmail.com>
2026-03-26 12:01:08 +0530

    Google tag Manager Added

--- app/layout.tsx
+++ app/layout.tsx
@@ -108,10 +108,6 @@ export default function RootLayout({
 				/>
 
 				{/* Google Analytics */}
-				<Script
-					src="https://www.googletagmanager.com/gtag/js?id=G-TTTFXCJ5RR"
-					strategy="afterInteractive"
-				/>
 
 				<Script id="google-analytics" strategy="afterInteractive">
 					{`
@@ -129,11 +125,35 @@ export default function RootLayout({
 						__html: JSON.stringify(schema),
 					}}
 				/>
+
+				{/* Google Tag Manager */}
+				<script
+				dangerouslySetInnerHTML={{
+					__html: `
+					(function(w,d,s,l,i){w[l]=w[l]||[];
+					w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+					var f=d.getElementsByTagName(s)[0],
+					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+					j.async=true;
+					j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+					f.parentNode.insertBefore(j,f);
+					})(window,document,'script','dataLayer','GTM-52Q5RR75');
+					`,
+				}}
+				/>
 			</head>
 
 			<body
 				className={`${sora.variable} ${marope.variable} ${redHatDisplay.variable} ${sanchez.variable} ${poppins.variable} antialiased`}
 			>
+				<noscript>
+					<iframe
+						src="https://www.googletagmanager.com/ns.html?id=GTM-52Q5RR75"
+						height="0"
+						width="0"
+						style={{ display: "none", visibility: "hidden" }}
+					></iframe>
+					</noscript>
 				<Providers>
 					{children}
 					<FloatingCTA />
